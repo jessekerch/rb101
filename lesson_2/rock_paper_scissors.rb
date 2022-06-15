d7a19a24042e0281c18af306a1867e7b9553acf6
Jesse Kercheval <jesse.kercheval@gmail.com>
2022-06-15 00:16:39 +0000

    updated rock paper scissors

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,19 +1,21 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors)
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer, scores)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("Yay, you WON this round!!")
     scores[:player] += 1
-  elsif (player == 'rock' && computer == 'paper') ||
-     (player == 'paper' && computer == 'scissors') ||
-     (player == 'scissors' && computer == 'rock')
-     prompt("Oh no, Computer won this round.")
+  elsif win?(computer, player)
+    prompt("Oh no, the Computer won this round!")
     scores[:computer] += 1
   else
     prompt("It's a tie!")
@@ -25,28 +27,30 @@ def display_score(scores)
   puts ""
 end
 
-score_hash = {player: 0, computer: 0}
+score_hash = { player: 0, computer: 0 }
 
 loop do # main loop
   choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = Kernel.gets().chomp()
-    
-      if VALID_CHOICES.include?(choice)
-        break
-      else
-        prompt("That's not a valid choice.")
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
     end
   end
-  
+
   computer_choice = VALID_CHOICES.sample
-  
-  prompt("You chose: #{choice.upcase}; Computer chose: #{computer_choice.upcase}")
-  
+
+  puts ""
+  prompt("You chose: #{choice.upcase};
+          Computer chose: #{computer_choice.upcase}")
+
   display_results(choice, computer_choice, score_hash)
   display_score(score_hash)
-  
+
   prompt("Do you want to play another round?")
   answer = Kernel.gets().chomp()
   system 'clear'
@@ -54,3 +58,4 @@ loop do # main loop
 end
 
 prompt("Thank you for playing. Goodbye!")
+display_score(score_hash)
